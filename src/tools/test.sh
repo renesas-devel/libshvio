7cb21d796bc650c0b64908edc1c9692ee3a656d9
Phil Edworthy <phil.edworthy@renesas.com>
2011-02-28 09:59:27 +0000

    Test: Add simple script to test VEU operations
    This is not an automated test, just a useful script to improve
    testing.

---
+++ src/tools/test.sh
@@ -0,0 +1,42 @@
+#!/bin/ash
+#
+# Script to test VEU operations using the tools
+
+
+# Check all format conversions
+for src in 888 rgb yuv x888; do \
+  for dst in 888 rgb yuv x888; do \
+    shveu-convert -s vga -S vga vga.${src} out_${src}.${dst}; \
+  done; \
+done
+
+# Display them
+for src in 888 rgb yuv x888; do \
+  for dst in 888 rgb yuv x888; do \
+    shveu-display -s vga out_${src}.${dst}; \
+  done; \
+done
+
+
+
+# Check rotation & mirroring across all formats
+for f in 0x0 0x1 0x2 0x30 0x10 0x20 0x11 0x21; do \
+  for src in 888 rgb yuv x888; do \
+    for dst in 888 rgb yuv x888; do \
+      shveu-convert -s vga -f $f vga.${src} out_${f}_${src}.${dst}; \
+    done; \
+  done; \
+done
+
+# HOST: Convert RGB24 files to png using ImageMagick
+cp /tftpboot/rootfs/root/*.888 .
+rename "s/888$/rgb/" *
+for dst in 888 rgb yuv x888; do \
+  for f in 0x0 0x30 0x10 0x20; do \
+    convert -size 640x480 -depth 8 out_${f}_${dst}.rgb  out_${f}_${dst}.png
+  done; \
+  for f in 0x1 0x2 0x11 0x21; do \
+    convert -size 480x640 -depth 8 out_${f}_${dst}.rgb  out_${f}_${dst}.png
+  done; \
+done
+
